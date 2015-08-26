b7394d46872350333afd42d4e6e133a81bc3b6a2
AntonioFilipov <tonkatafilipov@gmail.com>
2015-08-26 20:45:35 +0300

    add test structure

---
+++ Labyrinth_temp2_Tests/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Labyrinth_temp2_Tests
+{
+    [TestClass]
+    public class Game
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
