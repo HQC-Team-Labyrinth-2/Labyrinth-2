da7e9608c4dbc98ca1b9325d4eefd63064b652e1
AntonioFilipov <tonkatafilipov@gmail.com>
2015-09-29 22:27:15 +0300

    add console and file logger in Common/Logger

---
+++ Labyrinth-2-Structure/Labyrinth.Core/Common/Logger/ConsoleLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth.Core.Common.Logger
+{
+    public sealed class ConsoleLogger
+    {
+        private static ConsoleLogger logger = null;
+
+        private ConsoleLogger()
+        {
+        }
+
+        public static ConsoleLogger Instance()
+        {
+            if (logger == null)
+            {
+                logger = new ConsoleLogger();
+            }
+            return logger;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine("Time:{0} Message:{1}", DateTime.Now, message);
+        }
+    }
+}
---
+++ Labyrinth-2-Structure/Labyrinth.Core/Common/Logger/FileLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth.Core.Common.Logger
+{
+    public sealed class FileLogger
+    {
+        private static FileLogger logger = null;
+
+        private FileLogger()
+        {
+        }
+
+        public static FileLogger Instance()
+        {
+            if (logger == null)
+            {
+                logger = new FileLogger();
+            }
+            return logger;
+        }
+
+        public void Log(string message)
+        {
+            String textToLog = String.Format("Time:{0} Message:{1}\n", DateTime.Now, message);
+            File.AppendAllText("log.txt", textToLog);
+        }
+    }
+}
