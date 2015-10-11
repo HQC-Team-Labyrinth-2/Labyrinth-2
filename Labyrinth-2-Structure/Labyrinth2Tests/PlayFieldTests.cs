0e4266e57d39ad8116bd84022fd5c3aa012869ac
tima-t <tito.marchev@gmail.com>
2015-10-11 21:59:34 +0300

    Added tests for PlayField

---
+++ Labyrinth-2-Structure/Labyrinth2Tests/PlayFieldTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth2Tests
+{
+    using Labyrinth.Core.PlayField;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Labyrinth.Core.Helpers.Contracts;
+    using Labyrinth.Core.Helpers;
+    [TestClass]
+  public  class PlayFieldTests
+    {
+        [TestMethod]
+        public void TestPlayFieldWithCorrectData()
+        {
+            var position = new Position(0, 2);
+            var  generator = new StandardPlayFieldGenerator(position,3, 3);
+            PlayField field = new PlayField(generator,position,4,4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestPlayFieldWithIncorrectData()
+        {
+            var position = new Position(55, 55);
+            var generator = new StandardPlayFieldGenerator(position, 3, 3);
+            PlayField field = new PlayField(generator, position, 4, 4);
+        }
+
+
+
+         
+
+    }
+}
