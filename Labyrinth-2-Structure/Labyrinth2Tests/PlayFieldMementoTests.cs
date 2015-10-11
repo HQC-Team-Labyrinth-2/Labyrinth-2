7c7ad4b558407f8384446ea7cfb9d280e994896f
Pavel <pawel.angelow@gmail.com>
2015-10-11 22:40:44 +0300

    Added PlayFieldMementoTest

---
+++ Labyrinth-2-Structure/Labyrinth2Tests/PlayFieldMementoTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Labyrinth.Core.Common;
+using Labyrinth.Core.Helpers;
+using Labyrinth.Core.PlayField;
+using Labyrinth.Core.PlayField.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Labyrinth2Tests
+{
+    [TestClass]
+    public class PlayFieldMementoTests
+    {
+        [TestMethod]
+        public void TestPlayFieldProperyMustReturnCopyOfPlayfield()
+        {
+            var playField = new Cell[9, 9];
+            var position = new Position(3, 3);
+            var playFieldMemento = new PlayFieldMemento(playField, position);
+            Assert.AreNotEqual(playFieldMemento, playFieldMemento.PlayField);
+        }
+    }
+}
